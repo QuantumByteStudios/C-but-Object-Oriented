9f9dc70da029fac74e16b085aa33726d04af0d9a
QuantumByteStudios <quantumbytestudios@gmail.com>
2023-11-18 02:19:54 +0530

    Refactor code to implement object-oriented approach in C

--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include "utils.h"
 
+typedef struct
+{
+    // data members
+    char name[50];
+    int age;
+    // fuctions
+    void (*get)(struct Person *person);
+    void (*set)(struct Person *person, char *name, int age);
+} Person;
+
+// function definitions
+void get(Person *p)
+{
+    printf("Name: %s\n", p->name);
+    printf("Age: %d\n", p->age);
+}
+
+void set(Person *p, char *name, int age)
+{
+    strcpy(p->name, name);
+    p->age = age;
+}
+
 int main()
 {
     clear();
-    printf("Hello World!\n");
+
+    Person harshit, m;
+    harshit.get = get;
+    harshit.set = set;
+
+    harshit.set(&harshit, "Harshit", 20);
+    harshit.get(&harshit);
+
     return 0;
 }
